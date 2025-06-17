f8a2e6d9bd81d79c079fbccf25f3e8a9629d25f6
Balaji Kumar Varma G <balajikumarvarma768@gmail.com>
2025-06-17 12:26:18 +0530

    Update prog9.c

--- prog9.c
+++ prog9.c
@@ -1,62 +1,53 @@
 #include <stdio.h>
 
-#define MAX_N 100
-#define MAX_SUM 1000
+#define MAX 100
 
 int main() {
-    int n, d, S[MAX_N];
-    int dp[MAX_N + 1][MAX_SUM + 1];
-    int i, j;
+    int n, sum, a[MAX], dp[MAX][MAX] = {0};
 
-    printf("Enter number of elements in set: ");
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter the elements of the set:\n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &S[i]);
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 
-    printf("Enter the required sum: ");
-    scanf("%d", &d);
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
 
-    // Initialize DP table
-    for (i = 0; i <= n; i++)
-        dp[i][0] = 1; // Sum 0 is always possible (empty subset)
-    for (j = 1; j <= d; j++)
-        dp[0][j] = 0; // With 0 elements, no sum except 0 is possible
+    // Initialize: sum 0 is always possible
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
 
     // Fill DP table
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= d; j++) {
-            if (j < S[i-1])
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (j < a[i-1])
                 dp[i][j] = dp[i-1][j];
             else
-                dp[i][j] = dp[i-1][j] || dp[i-1][j - S[i-1]];
+                dp[i][j] = dp[i-1][j] || dp[i-1][j - a[i-1]];
         }
     }
 
-    // If no subset with sum d exists
-    if (!dp[n][d]) {
+    if (!dp[n][sum]) {
         printf("No subset found\n");
         return 0;
     }
 
-    // To print one subset, backtrack the DP table
-    printf("Subset with sum %d is: ", d);
-    i = n;
-    j = d;
+    // Backtrack to print one subset
+    printf("Subset with sum %d: ", sum);
+    int i = n, j = sum;
     while (i > 0 && j > 0) {
-        // If the sum j can be obtained without the current element,
-        // then the current element is not included.
-        if (dp[i-1][j])
+        if (dp[i-1][j]) {
             i--;
-        else {
-            // Else the current element is included.
-            printf("%d ", S[i-1]);
-            j -= S[i-1];
+        } else {
+            printf("%d ", a[i-1]);
+            j -= a[i-1];
             i--;
         }
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
+
